538985e8a37c09c29d94fd575383df50f16e11a2
Dashie <32211153+iluvjava@users.noreply.github.com>
2019-06-29 20:28:18 -0700

    stuff

---
+++ My SeeSharp Library Attempt/MyLibraryLaptopTestProj/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using APIs;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+        }
+    }
+}
