de19badabaffd2c64c61207a1a315d4f0f12b467
Dashie <32211153+iluvjava@users.noreply.github.com>
2019-09-22 19:58:13 -0700

    more

---
+++ My Frontend JS library/Some Example Usage of the Library/My TA Pages/my library (only functions).js
@@ -0,0 +1,122 @@
+(
+  () => {
+
+    /**
+     *  Read the object and change the object to elements with classes. 
+     */
+    function applyClassSettings(settings = SETTINGS) {
+      $.each(settings, function (idx, val) {
+        $(idx).addClass(val);
+      });
+    }
+
+    /**
+     * Prepare listeners from a json object. 
+     */
+    function prepareTheListeners(arg = LISTENER) {
+      $.each
+        (
+          arg,
+          function (k, v) {
+            $(k).on(v[0], v[1]);
+          }
+        )
+    }
+
+    /**
+     * Convert Json object to DOM elements. 
+     * @param {JSON} arg
+     * The JSon object has to be in particular grammartical syntax.
+     * @param {JQ DOM} parents  
+     * It's for the recursion so that children know what their parents are. 
+     */
+    function convert(arg = MYELEMENTS, parents = null) {
+      for (let i = 0; i < arg.length; i++) {
+        let obj = arg[i];
+        let NewDomMember;
+        if (
+          !("element" in obj)
+          ||
+          (parents === null && !("parent" in obj))
+        ) {
+          throw new Error("Invalid Json");
+        }
+        NewDomMember = $(createElement(obj["element"]));
+        if ("attributes" in obj) {
+          createAttrs(NewDomMember, obj["attributes"]);
+        }
+        if ("innertext" in obj) {
+          NewDomMember.text(obj["innertext"]);
+        }
+        if ("classlist" in obj) {
+          NewDomMember.addClass(obj["classlist"]);
+        }
+        if ("children" in obj) {
+          // debugger;
+          convert(obj["children"], NewDomMember);
+        }
+        let Parents = parents === null ? $(obj["parent"]) : parents;
+        Parents.append(NewDomMember);
+      }
+    }
+
+    /**
+     * This is a helper function for convert, It adds attrs to dom element
+     * except the ID attribute. 
+     * @param {JQ Dom object} domelement 
+     * The element created from JQ. 
+     * @param {JSON} jobject 
+     * The json object representing the attributes for the element.
+     */
+    function createAttrs(domelement, jobject) {
+
+      for (k in jobject) {
+        let v = jobject[k];
+        domelement.attr(k, v);
+      }
+    }
+
+    /**
+     * 
+     * @param {String} tagname 
+     */
+    function createElement(tagname) {
+      return document.createElement(tagname);
+    }
+
+
+    /**
+     * This function index all the children with a id. 
+     * If the given parent element already has an id, then 
+     * that id will be the prefix of its children.
+     * It will be done in a Non recursive fashion.
+     * @param {Dom Element} domelement 
+     * @parem {string} prefix
+     * the id will become prefix<index>, it's optional.
+     */
+    function indexChildrenWithId(domelement, prefix = null) {
+      let JQDomElement = $(domelement);
+      if (prefix === null) {
+        if (JQDomElement.attr("id") === undefined)
+          prefix = "";
+        else
+          prefix = JQDomElement.attr("id");
+      }
+      {
+        let counter = 0;
+        for (let c of JQDomElement.children()) {
+          c = $(c);
+          c.attr("id", prefix + counter);
+          counter++;
+        }
+      }
+      return JQDomElement;
+    }
+
+    window["applyClassSettings"] = applyClassSettings;
+    window["prepareTheListeners"] = prepareTheListeners;
+    window["convert"] = convert;
+    window["indexChildrenWithId"] = indexChildrenWithId;
+
+  }
+)();
